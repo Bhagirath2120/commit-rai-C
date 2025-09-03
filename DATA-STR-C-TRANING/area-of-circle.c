f5e710494122aa16e16f56f00e37a9e19fc22f04
Bhagirath2120 <bhagirathr2005@gami.com>
2025-09-03 15:36:27 +0530

    first commit

---
+++ DATA-STR-C-TRANING/area-of-circle.c
@@ -0,0 +1,13 @@
+// find the are of circle.
+
+#include <stdio.h>
+int main()
+{
+float radius, area;
+printf("Enter the radius of circle: ");
+scanf("%f", &radius);   
+area = 3.14 * radius * radius;
+printf("Area of circle = %f", area); 
+
+    return 0;
+}

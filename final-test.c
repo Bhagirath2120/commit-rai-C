dad6576c4e475aa91cbe9ae29aef9085a4a405b5
Bhagirath2120 <bhagirathr2005@gami.com>
2025-09-04 23:43:01 +0530

    happy

---
+++ final-test.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("Enter first Nunber = ");
+scanf("%d",&a);
+printf("\nEnter second Number = ");
+scanf("%d",&b);
+c = a+b;
+printf("Add of %d and %d = %d",a,b,c);
+return 0;
+
+
+}

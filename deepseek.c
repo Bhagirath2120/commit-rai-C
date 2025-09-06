1c550d5c9aaeccacef6068c45f9cda68479e0201
Bhagirath2120 <bhagirathr2005@gami.com>
2025-09-06 07:27:51 +0530

    divi-num

---
+++ deepseek.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int a = 10, b = 3;  // directly values assign करो
+    float c = (float)a/b;
+    printf("Div of %d and %d = %f", a, b, c);
+    return 0;
+}
--- divi-num.c
+++ divi-num.c
@@ -5,11 +5,14 @@ int main ()
 int a,b;
 float c;
 printf("Enter first number = ");
+fflush(stdout);
 scanf("%d",&a);
 printf("Enter second number = ");
+fflush(stdout);
 scanf("%d",&b);
 c = (float)a/b;
 printf("Div of %d and %d = %f",a,b,c);
+fflush(stdout);
 return 0;
 
 }
